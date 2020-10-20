4c77eff109e79c5c1a6cc6e1f88b6663cf4d994e
Eduardo Rodríguez <eduardostifler5@gmail.com>
2020-10-20 17:12:10 +0200

    Proyecto GGOT

---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './Homepage.scss';
+
+
+export default function HomePage () {
+
+
+    return (
+        <div className="c-backgroundImg">
+            <div className="row">
+                <div class="col-md-9">
+                <figcaption className="c-backgroundImg__caption">GAME OF THRONES</figcaption>
+            </div>
+            </div>
+        </div>
+    );
+}
